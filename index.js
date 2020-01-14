88bc1b891c8fd9f2be5d37170d664066b23066ca
Christian Bundy <christianbundy@fraction.io>
2020-01-13 18:50:36 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+const crypto = require("crypto")
+
+const keyPair = crypto.generateKeyPairSync("ed25519", {
+  publicKeyEncoding: {
+    type: "spki",
+    format: "der"
+  },
+  privateKeyEncoding: {
+    type: "pkcs8",
+    format: "der"
+  }
+})
+
+const publicKey = keyPair.publicKey.slice(-32).toString("base64")
+const author = `@${publicKey}.ed25519`
+
+const unsignedMessageObject = {
+  previous: null,
+  sequence: 1,
+  author,
+  timestamp: 1234567890987,
+  hash: "sha256",
+  content: {
+    type: "post",
+    text: "hello world"
+  }
+}
+
+const serialize = messageObject => JSON.stringify(messageObject, null, 2)
+const copyObject = object => JSON.parse(JSON.stringify(object))
+
+const unsignedMessageString = serialize(unsignedMessageObject)
+
+// TODO: Can we avoid passing through to `crypto.createPrivateKey()` here?
+const signature = crypto.sign(null, Buffer.from(unsignedMessageString), {
+  key: keyPair.privateKey,
+  format: "der",
+  type: "pkcs8"
+})
+
+const signedMessage = copyObject(unsignedMessageObject)
+signedMessage.signature = signature.toString("base64")
+
+console.log(serialize(signedMessage))
