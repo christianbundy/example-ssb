83e2e04b0e8c9a2166dc62727baae514a252c577
Christian Bundy <christianbundy@fraction.io>
2020-05-14 09:19:12 -0700

    Abstract away crypto operations

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 const crypto = require("crypto");
 
-let sequence = 1;
-let previous = null;
+const sha256 = (input) => crypto.createHash("sha256").update(input).digest();
 
-const createAuthor = () => {
-  // Create a pair of public and private keys, which will be used for:
-  //
-  // - Signing public messages.
-  // - Decrypting private messages.
-  //
-  // The public key is meant to be shared, the private key MUST NEVER BE SHARED.
+const createKeys = () => {
   const keyPair = crypto.generateKeyPairSync("ed25519", {
     publicKeyEncoding: {
       type: "spki",
@@ -21,10 +14,33 @@ const createAuthor = () => {
     },
   });
 
+  return {
+    getPublicKey: () => keyPair.publicKey.slice(-32),
+    sign: (input) =>
+      crypto.sign(null, Buffer.from(input), {
+        key: keyPair.privateKey,
+        format: "der",
+        type: "pkcs8",
+      }),
+  };
+};
+
+let sequence = 1;
+let previous = null;
+
+const createAuthor = () => {
+  // Create a pair of public and private keys, which will be used for:
+  //
+  // - Signing public messages.
+  // - Decrypting private messages.
+  //
+  // The public key is meant to be shared, the private key MUST NEVER BE SHARED.
+  const keys = createKeys();
+
   // I don't remember why we only want the last 32 characters, but we
   // need to convert it to base64 and then decorate it with the `@` prefix
   // and the `.ed25519` suffix.
-  const publicKeyBase64 = keyPair.publicKey.slice(-32).toString("base64");
+  const publicKeyBase64 = keys.getPublicKey().toString("base64");
   const author = `@${publicKeyBase64}.ed25519`;
 
   const messages = [];
@@ -43,24 +59,14 @@ const createAuthor = () => {
       // TODO: Can we avoid passing through to `crypto.createPrivateKey()` here?
       const payload = JSON.stringify(value, null, 2);
 
-      const signature = crypto
-        .sign(null, Buffer.from(payload), {
-          key: keyPair.privateKey,
-          format: "der",
-          type: "pkcs8",
-        })
-        .toString("base64");
+      const signature = keys.sign(payload).toString("base64");
 
       // The .ed25519 suffix is customary.
       value.signature = `${signature}.ed25519`;
 
       // We get the key *after* the signature. This has made a lot of people
       // very angry and been widely regarded as a bad move.
-      const hash = crypto
-        .createHash("sha256")
-        .update(JSON.stringify(value, null, 2))
-        .digest()
-        .toString("base64");
+      const hash = sha256(JSON.stringify(value, null, 2)).toString("base64");
 
       // The `%` prefix and `.sha256` suffix are customary.
       const key = `%${hash}.sha256`;
